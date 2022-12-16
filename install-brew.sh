b51daba386e15122df6f7b08337f816f6f541147
Jason Chaffee <jasonchaffee@gmail.com>
2022-12-15 20:07:44 -0800

    Update install-brew.sh

--- install-brew.sh
+++ install-brew.sh
@@ -3,6 +3,7 @@
 
 #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
 #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew analytics off
